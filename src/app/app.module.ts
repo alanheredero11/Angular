358190ab8c6a0798836465a370551a902f2a042b
alanheredero11@gmail.com <alanheredero11@gmail.com>
2022-11-29 11:49:50 +0100

    Header, footer and pages up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FormularioRegistroComponent } from './component/formulario-registro/formulario-registro.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    FormularioRegistroComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
